689464bd3acfa9daf7d5d1c381487be17fe6c95d
Angie <38557495+geulegoli@users.noreply.github.com>
2019-02-28 05:55:42 +0200

    Add files via upload

---
+++ Funcion6.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+Here you could calculate the different --> * = 
+<style>
+table,th,td {
+  border : 1px solid black;
+  border-collapse: collapse;
+}
+th,td {
+  padding: 5px;
+}
+
+
+</style>
+</head>
+<body>
+<script>
+function sumar()
+ {
+ var total = 0;
+ var valor1 = document.getElementById("val1").value;
+ var valor2 = document.getElementById("val2").value;
+
+ total = (valor1 * valor2);
+
+ var Display = document.getElementById("Display");
+ Display.innerHTML = total;
+ }
+</script>
+
+ <input id="val1" onkeyup="sumar()" />
+ <input id="val2" onkeyup="sumar()" />
+ <span id="Display"></span>
+<button type="button" onclick="loadXMLDoc()">Show 6 Page of Workers</button>
+<br><br>
+<table id="demo"></table>
+<script>
+function loadXMLDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+    }
+  };
+
+  xmlhttp.open("GET", "ListXML5.xml", true);
+  xmlhttp.send();
+}
+function myFunction(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var table="<tr><th>ID</th><th>Name</th><th>Date</th><th>Department</th><th>Post</th><th>Type</th><th>Hours</th><th>Prize</th><th>Wage</th></tr>";
+  var x = xmlDoc.getElementsByTagName("workers");
+  for (i = 0; i <x.length; i++) { 
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("ID")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("Name")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("Date")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("Department")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("Post")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("Type")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("Hours")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("Prize")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("Wage")[0].childNodes[0].nodeValue +
+    "</td></tr>"; 
+  }
+  document.getElementById("demo").innerHTML = table;
+}
+</script>
+</body>
+</html>
---
+++ Main.php
@@ -0,0 +1,34 @@
+<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ <link href="style.css" rel="stylesheet" type="text/css" />
+
+</head>
+<style>
+table,th,td {
+  border : 1px solid black;
+  border-collapse: collapse;
+}
+th,td {
+  padding: 5px;
+}
+</style>
+<body>
+
+
+
+</body>
+<footer>
+<ul class="horizontal">
+	<li><a href="Funcion1.php">Page 1(0-20)</a></li>
+	<li><a href="Funcion2.php">Page 2(20-40)</a></li>
+	<li><a href="Funcion3.php">Page 3(40-60)</a></li>
+	<li><a href="Funcion4.php">Page 4(60-80)</a></li>
+	<li><a href="Funcion5.php">Page 5(80-100)</a></li>
+	<li><a href="Funcion6.php">Page 6(0-40)</a></li>
+	<li><a href="Funcion7.php">Page 7(0-50)</a></li>
+	<li><a href="Funcion8.php">Page 8(0-100)</a></li>
+</ul>
+</footer>
+</html>
